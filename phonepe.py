fa8d124842ec8d2ab44c6a93425111686a203596
kuberan-35 <kuberankarthikeyan@gmail.com>
2025-06-27 23:29:16 +0530

    Update phonepe.py

--- phonepe.py
+++ phonepe.py
@@ -3,9 +3,9 @@ import pandas as pd
 import plotly.express as plotly
 import psycopg2
 
-engine = psycopg2.connect(
-        host="localhost", database="project phonepe", user="postgres", password="63693103k@"
-    )
+#engine = psycopg2.connect(
+        #host="localhost", database="project phonepe", user="postgres", password="63693103k@"
+   # )
 
 # ------------------------ PAGE SETUP ------------------------ #
 st.set_page_config(page_title="📱 PhonePe Data Insights", layout="wide")
