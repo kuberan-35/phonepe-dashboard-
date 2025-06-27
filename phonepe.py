1d8927f49ffc1cbd40a7bd0b5a88deb7d723f653
kuberan-35 <kuberankarthikeyan@gmail.com>
2025-06-27 23:54:03 +0530

    Update phonepe.py

--- phonepe.py
+++ phonepe.py
@@ -1,16 +1,16 @@
 
 import streamlit as st
-import pandas as pd
-#import plotly.express as plotly
-#import psycopg2
-
-#engine = psycopg2.connect(
- #       host="localhost", database="project phonepe", user="postgres", password="63693103k@"
-#)
 
 # ------------------------ PAGE SETUP ------------------------ #
-#st.set_page_config(page_title="📱 PhonePe Data Insights", layout="wide")
+st.set_page_config(page_title="📱 PhonePe Data Insights", layout="wide")
 st.title("📊 PhonePe Data Insights Dashboard")
 
 # ------------------------ FILTERS ------------------------ #     host="localhost", database="project phonepe", user="postgres", password="63693103k@
+import pandas as pd
+import plotly.express as plotly
+import psycopg2
+
+engine = psycopg2.connect(
+        host="localhost", database="project phonepe", user="postgres", password="63693103k@"
+)
 
