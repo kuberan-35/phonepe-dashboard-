2adcb37a9aba517a717a14249814524ccf0037ae
kuberan-35 <kuberankarthikeyan@gmail.com>
2025-06-28 11:40:42 +0530

    Update phonepe.py

--- phonepe.py
+++ phonepe.py
@@ -4,7 +4,7 @@ import plotly.express as plotly
 import psycopg2
 from sqlalchemy import create_engine
 # ------------------------ DATABASE CONNECTION ------------------------ #
- engine = create_engine("postgresql+psycopg2://postgres:63693103k@@localhost:5432/project phonepe")
+ engine = psycopg2.connect(create_engine("postgresql+psycopg2://postgres:63693103k@@localhost:5432/project phonepe"))
     
 # ------------------------ PAGE SETUP ------------------------ #
 st.set_page_config(page_title="📱 PhonePe Data Insights", layout="wide")
