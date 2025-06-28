d0d5522e7af85776c44742a0c7d25a8597ac7118
kuberan-35 <kuberankarthikeyan@gmail.com>
2025-06-29 00:06:54 +0530

    Update phonepe.py

--- phonepe.py
+++ phonepe.py
@@ -12,4 +12,13 @@ st.set_page_config(page_title="📱 PhonePe Data Insights", layout="wide")
 st.title("📊 PhonePe Data Insights Dashboard")
 
 # ------------------------ FILTERS ------------------------ #     host="localhost", database="project phonepe", user="postgres", password="63693103k@
+@st.cache_data
+def get_filter_options():
+    df_states = pd.read_sql("SELECT DISTINCT state FROM df_agg_user", engine)
+    df_years = pd.read_sql("SELECT DISTINCT year FROM df_agg_user", engine)
+    df_years = pd.read_sql("SELECT DISTINCT year FROM df_top_insurance", engine)
+    return sorted(df_states['state']), sorted(df_years['year'])
 
+states, years = get_filter_options()
+state_filter = st.sidebar.selectbox("Select State", states)
+year_filter = st.sidebar.selectbox("Select Year", years)
