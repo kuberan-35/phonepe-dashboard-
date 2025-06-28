c0a0b95d208d1ae2138ac9bcbeb2dc7c63732c0a
kuberan-35 <kuberankarthikeyan@gmail.com>
2025-06-29 00:05:51 +0530

    Update phonepe.py

--- phonepe.py
+++ phonepe.py
@@ -5,7 +5,7 @@ import psycopg2
 from sqlalchemy import create_engine
 # ------------------------ DATABASE CONNECTION ------------------------ #
 def get_engine(): 
-          psycopg2.connect(create_engine("postgresql+psycopg2://postgres:63693103k@@localhost:5432/project phonepe"))
+         return create_engine("postgresql+psycopg2://postgres:63693103k@@localhost:5432/project phonepe"))
 engine = get_engine()   
 # ------------------------ PAGE SETUP ------------------------ #
 st.set_page_config(page_title="📱 PhonePe Data Insights", layout="wide")
